470653fb3a6a011245e60f4ad1a90019191071f2
Cory Sullivan <cory@solbits.com>
2022-11-03 09:23:57 -0300

    add Sub type

--- Sources/TimeStackShared/SubscriptionType.swift
+++ Sources/TimeStackShared/SubscriptionType.swift
@@ -1,7 +1,9 @@
+// Copyright © 2022 Solbits Software Inc. All rights reserved.
+
 public enum SubscriptionType: String, Codable, Identifiable, Sendable {
   public var id: String {
     rawValue
   }
-  
+
   case account, calendar
 }
